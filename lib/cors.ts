f38345b9f33a764c86b05eabf426d8cd947f6cef
drmsr <me@drmsr.dev>
2024-01-18 21:07:23 +0800

    config cors

--- lib/cors.ts
+++ lib/cors.ts
@@ -1,4 +1,4 @@
 export default function getCors(url: string): string {
-	const corsUrl = process.env.CORS_PROXY + encodeURIComponent(url);
+	const corsUrl = process.env.NEXT_PUBLIC_CORS_PROXY + "/?" + encodeURIComponent(url);
 return corsUrl;
 }
