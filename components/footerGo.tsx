0ef13c149792d1b620707e35c86774721f0c5bac
drmsr <me@drmsr.dev>
2024-01-20 07:49:02 +0800

    fix twitter link

--- components/footerGo.tsx
+++ components/footerGo.tsx
@@ -5,7 +5,7 @@ const FooterGo = () => {
 	return (
 		<div id="footerGo" className="w-full flex flex-row items-center justify-center gap-5 p-5">
 			<Link href="https://www.facebook.com/sharer/sharer.php?u=https://solat.today" target="_blank"><Facebook size={20} color="#9CA3AF" className="hover:fill-gray-400"/></Link>
-			<Link href="http://www.twitter.com/share?url=https://solat.today" target="_blank"><Twitter size={20} color="#9CA3AF" className="hover:fill-gray-400" /></Link>
+			<Link href="https://twitter.com/intent/tweet?url=https%3A%2F%2Fsolat.today" target="_blank"><Twitter size={20} color="#9CA3AF" className="hover:fill-gray-400" /></Link>
 		</div>
 	)
 }
