e4bd37e324781178ce039b3418f9cef5122cf52e
drmsr <me@drmsr.dev>
2024-03-27 04:50:40 +0800

    fix index

--- lib/fetchers.ts
+++ lib/fetchers.ts
@@ -11,7 +11,7 @@ export async function getSiteData(domain: string) {
   return await unstable_cache(
     async () => {
       return prisma.site.findUnique({
-        where: subdomain ? { subdomain } : { customDomain: domain },
+        where: subdomain ? { subdomain } : { subdomain: domain },
         include: { user: true },
       });
     },
